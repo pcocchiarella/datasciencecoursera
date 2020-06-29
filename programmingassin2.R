2c5407cbda73a7aa251460505d8e249fc499d776
Page Cocchiarella <66397705+pcocchiarella@users.noreply.github.com>
2020-06-29 13:39:06 -0500

    Update programmingassign2.R
    Updated this assignment so that it has comments.

--- programmingassin2.R
+++ programmingassin2.R
@@ -1,3 +1,4 @@
+## I set x as a matrix and set s as null. Then I changed reference mean to "solve".
 makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
   s <- NULL
   set <- function(y) {
@@ -11,7 +12,7 @@ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
        setsolve = setsolve,
        getsolve = getsolve)
 }
-
+## Changed mean to "solve" and returned "s".
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
   if(!is.null(s)) {
